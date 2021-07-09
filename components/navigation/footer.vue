9b152a5a5219ffeba9acfb46bbe1010a53be79aa
raghibislam <islamraghib3@gmail.com>
2021-07-10 02:28:05 +0545

    Staging

---
+++ components/navigation/footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <b-row class="mx-sm-2">
+      <div class="col-sm-12 col-md-7 col-md-6 footer-text px-0">
+        Dealz DAO is an open source protocol and cryptocurrency leveraging
+        decentralized merit based governance to create trustless unbiased
+        agreement templates, legal task bots, and deal negotiation dApps.
+      </div>
+      <div class="ml-sm-0 ml-lg-auto mt-sm-4 mt-lg-0">
+        <div class="links">
+         
+        </div>
+        <div class="copyright-text">
+          Copyright Dealz 2021. All Rights Reserved.
+        </div>
+      </div>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import { BRow, BCol } from "bootstrap-vue";
+
+export default {
+  components: {
+    BRow,
+    BCol,
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+.footer-text {
+  font-size: 14px;
+  line-height: 24px;
+  color: #6b6b6b;
+}
+.links {
+  display: flex;
+  gap: 30px;
+  a {
+    color: #6b6b6b;
+    font-size: 14px;
+    text-decoration: none;
+  }
+}
+.copyright-text {
+  color: rgba(17, 17, 17, 0.32);
+  font-size: 12px;
+}
+.container {
+  @media (max-width: 650px) {
+    papdding: 20px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="landing">
+    <Navbar />
+    <Header />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { BCol, BContainer } from "bootstrap-vue";
+
+import Navbar from "../components/navigation/navbar.vue";
+import Header from "../components/landing/header.vue";
+import Footer from "../components/navigation/footer.vue";
+
+export default {
+  components: {
+    BCol,
+    BContainer,
+    Navbar,
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style lang="scss">
+.landing {
+  background-image: url("~static/landing-bg.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 1080px;
+  @media (max-width: 1920px) {
+    height: auto;
+  }
+}
+</style>
